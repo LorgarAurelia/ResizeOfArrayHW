de884f23b2d89078f8688047896d340eb0082670
David Maltsev <luterfaeron@gmail.com>
2021-08-02 16:43:17 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ResizeOfArrayHW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
